3ae02df394e7b3ebe89f62161eca0b63e51f937a
Erich Elste <erichelste@yahoo.com>
2022-05-30 16:17:10 -0400

    added event listeners in install.js, added putdb and get db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>{
+  console.log('Post to the ase');
+    const todosDb = await openDB('todos', 1);
+    const tx = todosDb.transaction('todos', 'readwrite');
+    const store = tx.objectStore('todos');
+    const request = store.add({ todo: content });
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  // console.error('putDb not implemented');
+} 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  console.log('GET from the database');
+  const contactDb = await openDB('contact', 1);
+  const tx = contactDb.transaction('contact', 'readonly');
+  const store = tx.objectStore('contact');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+  // console.error('getDb not implemented');
+} 
 
 initdb();
