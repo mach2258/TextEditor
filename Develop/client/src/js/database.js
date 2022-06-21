5194f0b9fd48cae2bcc752fe7a1093fbf6285e0c
Erich Elste <erichelste@yahoo.com>
2022-06-21 16:39:56 -0400

    pakage and databse.js issues

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('textEditor', 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('textEditor')) {
-        console.log('textEditor database already exists');
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('textEditor', { keyPath: 'id', autoIncrement: true });
-      console.log('textEditor database created');
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
@@ -21,7 +21,7 @@ export const putDb = async (content) =>{
     const request = store.add({ todo: content });
     const result = await request;
     console.log('🚀 - data saved to the database', result);
-} 
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
